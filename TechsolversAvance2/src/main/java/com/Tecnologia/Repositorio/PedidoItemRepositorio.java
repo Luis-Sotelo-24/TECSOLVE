af7829ef0b4a0160863671f99f1d356ba38e2bf9
Luis-Sotelo-24 <luissotelo351@gmail.com>
2025-06-26 06:10:59 -0500

    Se actualizo repositorio a las versionas funcionales

--- TechsolversAvance2/src/main/java/com/Tecnologia/Repositorio/PedidoItemRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.Tecnologia.Repositorio;
-
-import com.Tecnologia.Modelo.PedidoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface PedidoItemRepositorio extends JpaRepository<PedidoItem, Long> {
-    
-}
--- TechsolversAvance2/src/main/java/com/Tecnologia/Repositorio/PedidoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.Tecnologia.Repositorio;
-
-import com.Tecnologia.Modelo.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface PedidoRepositorio extends JpaRepository<Pedido, Long>{
-    
-}
