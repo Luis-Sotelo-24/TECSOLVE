2749db45b9744608a2df8cf7b2ac706b2a7f67f4
Luis-Sotelo-24 <luissotelo351@gmail.com>
2025-06-25 22:08:48 -0500

    Actualizacion del repositorio

---
+++ TecsolversAvance3/src/main/java/com/Tecnologia/Controladores/ControladorReclamos.java
@@ -0,0 +1,14 @@
+package com.Tecnologia.Controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorReclamos {
+    
+    @GetMapping("/reclamos")
+    public String Reclamos(Model modelo){
+        return "FrmReclamos";
+    }
+}
